747160d1cbf993cf2caff3137069ce76d0d60bbf
diemmoon <duongdiem20@gmail.com>
2021-11-01 08:12:14 +0700

    retry

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.example.orderservice.entity.Orders;
 import com.example.orderservice.repository.OrderRepository;
 import com.example.orderservice.vo.ResponseVO;
 import com.example.orderservice.vo.Users;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -22,14 +23,21 @@ public class OrderController {
     public List<Orders> getAll(){
         return  orderRepository.findAll();
     }
+
     @GetMapping("/{id}")
+    @Retry(name = "basic",fallbackMethod = "getFallBackOrders")
     public ResponseVO getById(@PathVariable long id){
         Orders o= orderRepository.findById(id).get();
         Users u= restTemplate.getForObject("/"+o.getUserid(), Users.class);
 
         return new ResponseVO(o,u) ;
     }
-
+    public ResponseVO getFallBackOrders(long id, RuntimeException runtimeException){
+        Orders o= orderRepository.findById(id).get();
+        ResponseVO v= new ResponseVO();
+        v.setOrders(o);
+        return  v;
+    }
     @PostMapping
     public  Orders add(@RequestBody Orders orders){
         return orderRepository.save(orders);
