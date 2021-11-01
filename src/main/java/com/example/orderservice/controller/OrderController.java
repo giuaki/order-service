9124050f66c9cdca00c8845b7d3d90d7e6dbaf7c
diemmoon <duongdiem20@gmail.com>
2021-11-01 08:24:23 +0700

    ratelimitter

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.example.orderservice.entity.Orders;
 import com.example.orderservice.repository.OrderRepository;
 import com.example.orderservice.vo.ResponseVO;
 import com.example.orderservice.vo.Users;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,7 @@ public class OrderController {
 
     @GetMapping("/{id}")
     @Retry(name = "basic",fallbackMethod = "getFallBackOrders")
+    @RateLimiter(name = "basic")
     public ResponseVO getById(@PathVariable long id){
         Orders o= orderRepository.findById(id).get();
         Users u= restTemplate.getForObject("/"+o.getUserid(), Users.class);
